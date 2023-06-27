575afd7cd1fa724e86eb9f9e73a0214d6e3f50b1
RyotaFukumoto <ryota.fukumoto@nextbeat.net>
2023-06-27 23:03:07 +0900

    Initial Commit

---
+++ SwiftUIScrollSample/SwiftUIScrollSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollSampleApp.swift
+//  SwiftUIScrollSample
+//
+//  Created by ryota.fukumoto on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
